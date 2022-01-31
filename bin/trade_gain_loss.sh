723c4ade205e992599edd9ef5b1e6cfae688d275
nycnighthawk <nycnighthawk@local.local>
2022-01-30 20:10:15 -0500

    updated venv for my_trades, updated scripts

--- bin/trade_gain_loss.sh
+++ bin/trade_gain_loss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-my_trades_venv=${HOME}/projects/my_trades/.venv
+my_trades_venv=${HOME}/projects/my_trades/venv
 source ${my_trades_venv}/bin/activate
 python -m my_trades.gain_loss "$@"
