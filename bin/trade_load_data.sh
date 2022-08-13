c27110f7633cebe5f1b1f7c06528ea7381fbe77c
nycnighthawk <nycnighthawk@local.local>
2022-08-13 10:00:18 -0400

    refactored my_bash, updated trade script

--- bin/trade_load_data.sh
+++ bin/trade_load_data.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-my_trades_venv=${HOME}/projects/my_trades/venv
-source ${my_trades_venv}/bin/activate
+if [ -d "${HOME}/projects/my_trades/venv" ]
+then
+    my_trades_venv="${HOME}/projects/my_trades/venv"
+elif [ -d "${HOME}/projects/my_trades/.venv" ]
+then
+    my_trades_venv="${HOME}/projects/my_trades/.venv"
+fi
+. "${my_trades_venv}/bin/activate"
 python -m my_trades.database "$@"
