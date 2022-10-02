57c315205fff24d1a7e2225b6f2323e87842d70b
nycnighthawk <nycnighthawk@local.local>
2022-10-02 10:47:03 -0400

    refactored update-system.sh with dedicated function for apt-get

--- bin/update-system.sh
+++ bin/update-system.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-command -v apt &> /dev/null && \
-    apt-get -y update && \
-    apt-get -y upgrade && \
-    apt-get -y dist-upgrade && \
+apt_get_options='-o "Acquire::https::Verify-Peer=false"'
+apt_get_options=${apt_get_options}' -o "Acquire::https::Verify-Host=false"'
+apt_get_options=${apt_get_options}' -o "APT::Get::AllowUnauthenticated=true"'
+apt_get_options=${apt_get_options}' -o "Acquire::AllowInsecureRepositories=true"'
+apt_get_options=${apt_get_options}' -o "Acquire::AllowDowngradeToInsecureRepositories=true"'
+apt_get_options=${apt_get_options}' --allow-unauthenticated'
+
+if [ -z ${APT_GET_INSECURE} ]
+then
+    apt_get_options=
+fi
+
+update_with_apt_get() {
+    local apt_get_options=$1
+    echo "apt_get_options: ${apt_get_options}"
+    apt-get -y ${apt_get_options} update && \
+    apt-get -y ${apt_get_options} upgrade && \
+    apt-get -y ${apt_get_options} dist-upgrade && \
     apt-get -y autoremove
+}
+
+command -v apt &> /dev/null && \
+    update_with_apt_get ${apt_get_options}
 
 command -v yum &> /dev/null && \
     yum -y update
-
