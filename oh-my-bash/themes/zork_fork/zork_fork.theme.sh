a3a96c4d62594ffb89fa89e026b97e61ec58199d
nycnighthawk <nycnighthawk@local.local>
2022-10-31 13:39:01 -0400

    Updated oh-my-bash zork_fork theme

--- oh-my-bash/themes/zork_fork/zork_fork.theme.sh
+++ oh-my-bash/themes/zork_fork/zork_fork.theme.sh
@@ -22,6 +22,11 @@ esac
 
 PS3=">> "
 
+# yellow
+_my_prompt_color='\[\e[0;33m\]'
+# blue
+_my_blue_color='\[\e[1;34m\]'
+
 __my_rvm_ruby_version() {
     local gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
   [ "$gemset" != "" ] && gemset="@$gemset"
@@ -60,8 +65,9 @@ chroot(){
 my_ve(){
     if [ -n "$VIRTUAL_ENV" ]
     then 
-        my_ps_ve="${_omb_prompt_bold_purple}$ve${_omb_prompt_normal}";
-        echo "($my_ps_ve)";
+        ve=`basename $VIRTUAL_ENV`
+        my_ps_ve="${_my_blue_color}[${_omb_prompt_bold_purple}$ve${_my_blue_color}]${_omb_prompt_normal}";
+        echo "$my_ps_ve";
     fi
     echo "";
     }
@@ -71,36 +77,27 @@ date_prompt() {
     echo "${_d}"
 }
 
-# yellow
-_my_prompt_color='\[\e[0;33m\]'
-# blue
-_my_blue_color='\[\e[1;34m\]'
 
 _omb_theme_PROMPT_COMMAND() {
 
     # my_ps_host="${_omb_prompt_green}\h $(date_prompt)${_omb_prompt_normal}";
-    my_ps_host="${_my_prompt_color}\h $(date_prompt)${_omb_prompt_normal}";
+    my_ps_host="${_my_prompt_color}\h${_omb_prompt_normal}";
     # yes, these are the the same for now ...
     # my_ps_host_root="${_omb_prompt_green}\h $(date_prompt)${_omb_prompt_normal}";
-    my_ps_host_root="${_my_prompt_color}\h $(date_prompt)${_omb_prompt_normal}";
+    my_ps_host_root="${_my_prompt_color}\h${_omb_prompt_normal}";
  
     # my_ps_user="${_omb_prompt_bold_green}\u@${_omb_prompt_normal}"
     my_ps_user="${_my_prompt_color}\u@${_omb_prompt_normal}"
     # my_ps_root="${_omb_prompt_bold_brown}\u@${_omb_prompt_normal}";
     my_ps_root="${_my_prompt_color}\u@${_omb_prompt_normal}";
 
-    if [ -n "$VIRTUAL_ENV" ]
-    then
-        ve=`basename $VIRTUAL_ENV`;
-    fi
-
     # nice prompt
     case "`id -u`" in
-        0) PS1="${TITLEBAR}${_my_blue_color}┌─${_omb_prompt_normal}$(my_ve)$(chroot)${_my_blue_color}[$my_ps_user$my_ps_host_root${_my_blue_color}]$(modern_scm_prompt)$(__my_rvm_ruby_version)$(is_vim_shell)
+        0) PS1="${TITLEBAR}${_my_blue_color}┌─${_omb_prompt_normal}$(chroot)${_my_blue_color}[$my_ps_user$my_ps_host_root${_my_blue_color}][${_my_prompt_color}$(date_prompt)${_my_blue_color}]${_omb_prompt_normal}$(my_ve)$(modern_scm_prompt)$(__my_rvm_ruby_version)$(is_vim_shell)
 ${_my_blue_color}│ [${_omb_prompt_normal}\w${_my_blue_color}]
 ${_my_blue_color}└─#${_omb_prompt_normal} "
         ;;
-        *) PS1="${TITLEBAR}${_my_blue_color}┌─${_omb_prompt_normal}$(my_ve)$(chroot)${_my_blue_color}[$my_ps_user$my_ps_host${_my_blue_color}]$(modern_scm_prompt)$(__my_rvm_ruby_version)$(is_vim_shell)
+    *) PS1="${TITLEBAR}${_my_blue_color}┌─${_omb_prompt_normal}$(chroot)${_my_blue_color}[$my_ps_user$my_ps_host${_my_blue_color}][${_my_prompt_color}$(date_prompt)${_my_blue_color}]${_omb_prompt_normal}$(my_ve)$(modern_scm_prompt)$(__my_rvm_ruby_version)$(is_vim_shell)
 ${_my_blue_color}│ [${_omb_prompt_normal}\w${_my_blue_color}] 
 ${_my_blue_color}└─\$${_omb_prompt_normal} "
         ;;
