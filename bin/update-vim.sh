daf4600abad83bab1d7ca170c153b160fa02ae80
nycnighthawk <nycnighthawk@local.local>
2023-02-20 12:03:49 -0500

    - fix vimrc wildmode, wildmenu settings - updated update-vim.sh script to use functions - updated coc-settings.json for coc.nvim

--- bin/update-vim.sh
+++ bin/update-vim.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
+#
+prog_name=$(basename $0)
 
-# install vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+show_usage() {
+    help_message=`cat \
+<<- EOF
+Usage: [NO_SSL_VERIFY=1] ${prog_name} [-h]
 
-vim +PlugUpdate +PlugUpgrade +qall
-vim +CocUpdateSync +qall
+This script update the vim plug and Coc
+
+EOF
+`
+    echo "${help_message}"
+}
+
+current_git_ssl_setting=$(git config --global --get http.sslverify)
+
+set_options() {
+    if [ "${NO_SSL_VERIFY}" = "1" ]
+    then
+        export NODE_TLS_REJECT_UNAUTHORIZED=0
+        export NPM_CONFIG_STRICT_SSL=false
+        git config --global --replace http.sslverify false
+    fi
+}
+
+restore_options() {
+    unset NODE_TLS_REJECT_UNAUTHORIZED
+    unset NPM_CONFIG_STRICT_SSL
+    if [ -n "${current_git_ssl_setting}" ]
+    then
+        git config --global --replace http.sslverify ${current_git_ssl_setting}
+    fi
+}
+
+main() {
+    set_options
+    vim +PlugUpdate +PlugUpgrade +qall
+    vim +CocUpdateSync +qall
+    restore_options
+}
+
+case $1 in
+    -h|--h)
+    show_usage
+    exit 0
+    ;;
+esac
+main
